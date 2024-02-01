53e415ac6f660334a0c2b09f7a012b53e4555c93
jangwonseok <jangws1003@naver.com>
2024-02-01 15:18:06 +0900

    filter 구조 add

---
+++ src/main/java/com/origemite/gateway/conf/ConfigFilter.java
@@ -0,0 +1,20 @@
+package com.origemite.gateway.conf;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigFilter {
+    //https://spring.io/projects/spring-cloud-gateway
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r.path("/auth-service/**")
+                        .filters(f   -> f.rewritePath("/auth-service/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://auth-service"))
+                .build();
+    }
+
+}
