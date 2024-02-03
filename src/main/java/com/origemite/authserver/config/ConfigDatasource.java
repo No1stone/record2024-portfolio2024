8500f1872d72833d7d1d28b1a90feac2bcf37ab9
jangwonseok <jangws1003@naver.com>
2024-02-03 18:19:22 +0900

    feat(auth) : 데이터 소스 베이스

---
+++ src/main/java/com/origemite/authserver/config/ConfigDatasource.java
@@ -0,0 +1,97 @@
+package com.origemite.authserver.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableScheduling
+@EnableTransactionManagement
+@EnableJpaRepositories("com.origemite.authserver.data.db.repo")
+public class ConfigDatasource {
+
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String userName;
+    @Value("${spring.datasource.password}")
+    private String passVal;
+    private String tags = "#";
+    @Value("${spring.jpa.properties.hibernate.dialect}")
+    private String dialect;
+    @Value("${spring.jpa.show_sql}")
+    private boolean showSql;
+    @Value("${spring.jpa.format_sql}")
+    private boolean formatSql;
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String hbm2ddlAuto;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    private String validationQuery = "SELECT 1";
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder
+                .create()
+                .driverClassName(driverClassName)
+                .url(url)
+                .username(userName)
+                .password(passVal)
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+
+        return txManager;
+    }
+
+    @Bean
+    public Properties hibernateProperties() {
+        // println형태의 콘솔출력을 사용할때
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", dialect);
+//        properties.put("hibernate.show_sql", showSql);
+//        properties.put("hibernate.format_sql", formatSql);
+//        properties.put("hibernate.hbm2ddl.auto", hbm2ddlAuto);
+        return properties;
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("com.origemite.authserver.data.db.entity");
+        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//		vendorAdapter.setShowSql(true);
+//		vendorAdapter.setGenerateDdl(true);
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(hibernateProperties());
+        em.afterPropertiesSet();
+
+        return em.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(this.entityManagerFactory());
+        return transactionManager;
+    }
+
+}
