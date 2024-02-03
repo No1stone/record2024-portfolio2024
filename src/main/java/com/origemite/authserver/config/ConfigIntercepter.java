6b68c40a5c8f10ff68485a51529385250beffcbe
jangwonseok <jangws1003@naver.com>
2024-02-03 18:19:31 +0900

    feat(auth) : 인터셉터 베이스

---
+++ src/main/java/com/origemite/authserver/config/ConfigIntercepter.java
@@ -0,0 +1,22 @@
+package com.origemite.authserver.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ConfigIntercepter implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getMethod().equals("OPTIONS")) {
+            return true;
+        }
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
