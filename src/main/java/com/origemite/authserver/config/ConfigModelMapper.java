f401e1aab6a2909fb38183622d70f04f858455c2
jangwonseok <jangws1003@naver.com>
2024-02-03 18:19:53 +0900

    feat(auth) : 모델매퍼 콘피

---
+++ src/main/java/com/origemite/authserver/config/ConfigModelMapper.java
@@ -0,0 +1,38 @@
+package com.origemite.authserver.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigModelMapper {
+
+
+    @Bean
+    public ModelMapper strictMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+	/*
+	@Bean
+	public ModelMapper StandardMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+		.setMatchingStrategy(MatchingStrategies.STANDARD);
+		return modelMapper;
+	}
+	*/
+	/*
+	@Bean
+	public ModelMapper LooseMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+		.setMatchingStrategy(MatchingStrategies.LOOSE);
+		return modelMapper;
+	}
+	*/
+}
+
