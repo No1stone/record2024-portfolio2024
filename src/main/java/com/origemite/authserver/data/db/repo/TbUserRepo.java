d84ac4306706e6d2b7fd443cb528f8bdc36d30e4
jangwonseok <jangws1003@naver.com>
2024-02-05 16:07:24 +0900

    feat(auth) : 리포 이메일검색 추가

--- src/main/java/com/origemite/authserver/data/db/repo/TbUserRepo.java
+++ src/main/java/com/origemite/authserver/data/db/repo/TbUserRepo.java
@@ -3,5 +3,10 @@ package com.origemite.authserver.data.db.repo;
 import com.origemite.authserver.data.db.entity.TbUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TbUserRepo extends JpaRepository<TbUser, String> {
+
+    Optional<TbUser> findByUsrEmail(String email);
+
 }
