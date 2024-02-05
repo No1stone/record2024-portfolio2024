bfba73bbbdbb1fd6aece19c2ec48b143123ff77e
jangwonseok <jangws1003@naver.com>
2024-02-05 13:46:13 +0900

    feat(auth) : 유저컨트롤러 추가

---
+++ src/main/java/com/origemite/authserver/biz/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.origemite.authserver.biz.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/v1/user")
+public class UserController {
+}
