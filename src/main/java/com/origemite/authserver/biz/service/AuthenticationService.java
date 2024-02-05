b373a74f0df1830b4a2b0fc5a72edb125c62e6c0
jangwonseok <jangws1003@naver.com>
2024-02-05 17:17:58 +0900

    feat(auth) : 인증 수정

--- src/main/java/com/origemite/authserver/biz/service/AuthenticationService.java
+++ src/main/java/com/origemite/authserver/biz/service/AuthenticationService.java
@@ -1,7 +1,9 @@
 package com.origemite.authserver.biz.service;
 
+import com.google.gson.Gson;
 import com.origemite.authserver.biz.controller.vo.ReqSignin;
 import com.origemite.authserver.biz.controller.vo.ReqSignup;
+import com.origemite.authserver.config.ConfigPasswordEncoder;
 import com.origemite.authserver.data.db.entity.TbUser;
 import com.origemite.authserver.data.db.repo.TbUserRepo;
 import lombok.RequiredArgsConstructor;
@@ -18,20 +20,22 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
     private final TbUserRepo tbUserRepo;
+    private final ConfigPasswordEncoder passwordEncoder;
 
 
     public String signin(ReqSignin request) {
-        String result = "";
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
         var user = tbUserRepo.findByUsrEmail(request.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsrId(), request.getPassword()));
         var jwt = jwtService.generateAccessToken(user.getUsrId());
-        return result;
+        return jwt;
     }
 
     public String signup(ReqSignup request) {
         String result = "";
+
+        request.toEncoder(passwordEncoder);
 
         return result;
     }
