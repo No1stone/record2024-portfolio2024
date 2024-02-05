f5515787853c2959a3fd98e3035e6aa924c1fee2
jangwonseok <jangws1003@naver.com>
2024-02-05 16:41:45 +0900

    feat(auth) : 사인업 추가중

--- src/main/java/com/origemite/authserver/biz/controller/vo/ReqSignup.java
+++ src/main/java/com/origemite/authserver/biz/controller/vo/ReqSignup.java
@@ -1,14 +1,26 @@
 package com.origemite.authserver.biz.controller.vo;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.origemite.authserver.config.ConfigPasswordEncoder;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ReqSignup {
 
+    @Email
+    private String email;
+    private String password;
 
+    public ReqSignup toEncoder(ConfigPasswordEncoder encoder) {
+        return ReqSignup.builder()
+                .email(this.email)
+                .password(encoder.passwordEncoder().encode(this.password))
+                .build();
+    }
 
 }
