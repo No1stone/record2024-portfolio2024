583cbab2a093e818e836278536d66f00a6c43ea3
jangwonseok <jangws1003@naver.com>
2024-02-05 16:05:56 +0900

    feat(auth) : 회원가입 로그인 컨트롤러

---
+++ src/main/java/com/origemite/authserver/biz/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.origemite.authserver.biz.controller;
+
+import com.origemite.authserver.biz.controller.vo.ReqSignin;
+import com.origemite.authserver.biz.controller.vo.ReqSignup;
+import com.origemite.authserver.biz.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+    @PostMapping("/signup")
+    public String signup(@RequestBody ReqSignup request) {
+        return authenticationService.signup(request);
+    }
+
+    @PostMapping("/signin")
+    public String signin(@RequestBody ReqSignin request) {
+        return authenticationService.signin(request);
+    }
+
+}
