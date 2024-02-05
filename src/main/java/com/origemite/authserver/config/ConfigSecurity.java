488d74267bed694aec968ad2eb421b5f7d21ad26
jangwonseok <jangws1003@naver.com>
2024-02-05 16:06:20 +0900

    feat(auth) : 시큐리티 추가

--- src/main/java/com/origemite/authserver/config/ConfigSecurity.java
+++ src/main/java/com/origemite/authserver/config/ConfigSecurity.java
@@ -1,42 +1,42 @@
 package com.origemite.authserver.config;
 
+import com.origemite.authserver.cmm.JwtAuthenticationFilter;
 import com.origemite.authserver.biz.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
-@Component
+@Configuration
 @RequiredArgsConstructor
 public class ConfigSecurity {
 
     private final UserService userService;
     private final ConfigPasswordEncoder configPasswordEncoder;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     //WebSecurityConfigurerAdapter 디프리케이트 후
     //참조소스
     //https://stackoverflow.com/questions/72381114/spring-security-upgrading-the-deprecated-websecurityconfigureradapter-in-spring
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/**")
-                        .permitAll())
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**")
+                        .permitAll().anyRequest().authenticated())
 //                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**")
 //                        .permitAll().anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authenticationProvider(authenticationProvider()).addFilterBefore(
-//                        jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .authenticationProvider(authenticationProvider()).addFilterBefore(
+                        jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         ;
         return http.build();
     }
