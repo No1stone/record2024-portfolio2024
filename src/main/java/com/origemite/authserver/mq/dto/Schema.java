d4d0974e614b7fd1d95ace6def84cc747e5c2ca9
jangwonseok <jangws1003@naver.com>
2024-02-06 16:28:52 +0900

    feat(auth) : 스키마 밸류오브젝트

---
+++ src/main/java/com/origemite/authserver/mq/dto/Schema.java
@@ -0,0 +1,39 @@
+package com.origemite.authserver.mq.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@Builder
+public class Schema {
+
+    private String type;
+    private List<Field> fields;
+    private boolean optional;
+    private String name;
+
+    public static Schema UserSchema(){
+        List<Field> fields = Arrays.asList(
+            new Field("String", true, "USR_ID")
+                , new Field("string",true, "CTM_ID")
+                , new Field("string",true, "USR_EMAIL")
+                , new Field("string",true, "USR_PASSWORD")
+                , new Field("string",true, "USR_NAME")
+                , new Field("string",true, "USR_DESC")
+                , new Field("string",true, "USR_MOBILE")
+                , new Field("int32",true, "USR_ROLE")
+                , new Field("int64",true, "CREATE_AT")
+                , new Field("int64",true, "UPDATE_AT")
+        );
+        return Schema.builder()
+                .type("struct")
+                .fields(fields)
+                .optional(false)
+                .name("TB_USER")
+                .build();
+    }
+
+}
