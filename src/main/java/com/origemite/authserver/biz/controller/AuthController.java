886e0fe5c04f621a8316096fb5727758210862be
jangwonseok <jangws1003@naver.com>
2024-02-08 16:02:48 +0900

    feat(auth) : 사인업 개발

--- src/main/java/com/origemite/authserver/biz/controller/AuthController.java
+++ src/main/java/com/origemite/authserver/biz/controller/AuthController.java
@@ -1,30 +1,33 @@
 package com.origemite.authserver.biz.controller;
 
+import com.origemite.authserver.advice.excep.CustomBadRequestException;
 import com.origemite.authserver.biz.controller.vo.ReqSignin;
 import com.origemite.authserver.biz.controller.vo.ReqSignup;
+import com.origemite.authserver.biz.controller.vo.ResToken;
 import com.origemite.authserver.biz.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/token")
+@CrossOrigin
 public class AuthController {
 
     private final AuthenticationService authenticationService;
-    @PostMapping("/signup")
-    public String signup(@RequestBody ReqSignup request) {
-        return authenticationService.signup(request);
-    }
 
     @PostMapping("/signin")
-    public String signin(@RequestBody ReqSignin request) {
-        return authenticationService.signin(request);
+    public ResToken OrigemiteSignin(@RequestBody ReqSignin request) {
+        return ResToken.builder().token(authenticationService.signin(request)).build();
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<ResToken> OrigemiteSignup(@RequestBody ReqSignup request) throws CustomBadRequestException {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResToken.builder().token(authenticationService.signup(request)).build());
     }
 
 }
