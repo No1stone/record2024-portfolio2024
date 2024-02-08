a8ae8885bf867d2573ea8100b13f08c92af34a51
jangwonseok <jangws1003@naver.com>
2024-02-08 16:05:37 +0900

    feat(auth) : 토큰 브이오

---
+++ src/main/java/com/origemite/authserver/biz/controller/vo/ResToken.java
@@ -0,0 +1,14 @@
+package com.origemite.authserver.biz.controller.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResToken {
+    private String token;
+}
