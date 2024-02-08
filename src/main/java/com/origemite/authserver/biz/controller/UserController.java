9f48ca578366e219c9673af7f6e80a73d9d88d2e
jangwonseok <jangws1003@naver.com>
2024-02-08 13:01:50 +0900

    feat(auth) : 컨트롤러

--- src/main/java/com/origemite/authserver/biz/controller/UserController.java
+++ src/main/java/com/origemite/authserver/biz/controller/UserController.java
@@ -2,12 +2,21 @@ package com.origemite.authserver.biz.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/user")
+@CrossOrigin
 public class UserController {
+
+    @PostMapping({"","/"})
+    public ResponseEntity UserVreate(){
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
 }
