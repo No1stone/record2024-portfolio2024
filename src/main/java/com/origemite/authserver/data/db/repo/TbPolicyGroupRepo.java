b76917c6ee55a3f575bcad12b16fbcea6479c6be
jangwonseok <jangws1003@naver.com>
2024-02-09 21:32:06 +0900

    feat(auth) : 쿼리 수정

--- src/main/java/com/origemite/authserver/data/db/repo/TbPolicyGroupRepo.java
+++ src/main/java/com/origemite/authserver/data/db/repo/TbPolicyGroupRepo.java
@@ -4,8 +4,13 @@ import com.origemite.authserver.data.db.entity.TbPolicyGroup;
 import com.origemite.authserver.data.db.entity.id.TbPolicyGroupId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TbPolicyGroupRepo extends JpaRepository<TbPolicyGroup, TbPolicyGroupId> {
-    Optional<TbPolicyGroup> findByPlcId(String plcId);
+    List<TbPolicyGroup> findByPlcId(String plcId);
+    List<TbPolicyGroup> findBySvcId(String svcId);
+    List<TbPolicyGroup> findByPlcIdAndSvcId(String plcId, String svcId);
+
+
 }
