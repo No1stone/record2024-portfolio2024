4fd83292d6564b31321ecd3abee051f9541b7dcd
jangwonseok <jangws1003@naver.com>
2024-02-09 21:29:58 +0900

    feat(auth) : 조회 로직 추가

--- src/main/java/com/origemite/authserver/biz/service/PolicyGroupService.java
+++ src/main/java/com/origemite/authserver/biz/service/PolicyGroupService.java
@@ -1,14 +1,19 @@
 package com.origemite.authserver.biz.service;
 
 import com.origemite.authserver.advice.excep.CustomNotFoundException;
-import com.origemite.authserver.biz.controller.policy.vo.ReqPolicyGroupSave;
-import com.origemite.authserver.biz.controller.policy.vo.ReqPolicyGroupSelect;
-import com.origemite.authserver.biz.controller.policy.vo.ReqPolicySave;
-import com.origemite.authserver.biz.controller.policy.vo.ResPolicySelect;
+import com.origemite.authserver.advice.excep.CustomRuntimeException;
+import com.origemite.authserver.biz.controller.policy.vo.*;
+import com.origemite.authserver.cmm.ServiceCode;
+import com.origemite.authserver.data.db.entity.TbPolicyGroup;
 import com.origemite.authserver.data.db.repo.TbPolicyGroupRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -22,7 +27,32 @@ public class PolicyGroupService {
         tbPolicyGroupRepo.save(dto.toTbPolicyGroup());
     }
 
-    public ReqPolicyGroupSelect PolicyGroupSelect(String plcId) throws CustomNotFoundException {
-       return tbPolicyGroupRepo.findByPlcId(plcId).orElseThrow(() -> new CustomNotFoundException()).toTbPolicyGroup();
+    public List<ResPolicyGroupSelect> PolicyGroupSelect(ReqPolicyGroupSelect dto) throws CustomNotFoundException {
+        boolean chkPlc = false;
+        boolean chkSvc = false;
+        List<ResPolicyGroupSelect> result = new ArrayList<>();
+        if (dto.getPlcId() != null) {
+            chkPlc = true;
+        }
+        if (dto.getSvcId() != null) {
+            if(!ServiceCode.LIST.stream().map(e -> e.name()).collect(Collectors.toList()).contains(dto.getSvcId())){
+                throw new CustomNotFoundException("서비스 코드를 확인해주세요");
+            }
+            chkSvc = true;
+        }
+        if (chkPlc && chkSvc) {
+            result = tbPolicyGroupRepo.findByPlcIdAndSvcId(dto.getPlcId(), dto.getSvcId())
+                    .stream().map(TbPolicyGroup::toTbPolicyGroup).collect(Collectors.toList());
+        } else {
+            if (chkPlc) {
+                result = tbPolicyGroupRepo.findByPlcId(dto.getPlcId())
+                        .stream().map(TbPolicyGroup::toTbPolicyGroup).collect(Collectors.toList());
+            }
+            if (chkSvc) {
+                result = tbPolicyGroupRepo.findBySvcId(dto.getSvcId())
+                        .stream().map(TbPolicyGroup::toTbPolicyGroup).collect(Collectors.toList());
+            }
+        }
+        return result;
     }
 }
