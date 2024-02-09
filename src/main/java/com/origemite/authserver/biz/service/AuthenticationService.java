7586ba0dd898cff1d732a12987ac41bb07d5d6da
jangwonseok <jangws1003@naver.com>
2024-02-09 15:45:20 +0900

    feat(auth) : package 수정

--- src/main/java/com/origemite/authserver/biz/service/AuthenticationService.java
+++ src/main/java/com/origemite/authserver/biz/service/AuthenticationService.java
@@ -1,9 +1,8 @@
 package com.origemite.authserver.biz.service;
 
-import com.google.gson.Gson;
 import com.origemite.authserver.advice.excep.CustomBadRequestException;
-import com.origemite.authserver.biz.controller.vo.ReqSignin;
-import com.origemite.authserver.biz.controller.vo.ReqSignup;
+import com.origemite.authserver.biz.controller.auth.vo.ReqSignin;
+import com.origemite.authserver.biz.controller.auth.vo.ReqSignup;
 import com.origemite.authserver.config.ConfigPasswordEncoder;
 import com.origemite.authserver.data.db.entity.TbUser;
 import com.origemite.authserver.data.db.repo.TbUserRepo;
