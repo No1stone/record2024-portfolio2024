87050defb56a5bdd679bb1d9675747836f0ab24c
jangwonseok <jangws1003@naver.com>
2024-02-10 17:17:35 +0900

    feat(auth) : 유저 정책 조회 쿼리

--- src/main/java/com/origemite/authserver/data/db/repo/TbUserPolicyRepo.java
+++ src/main/java/com/origemite/authserver/data/db/repo/TbUserPolicyRepo.java
@@ -4,6 +4,10 @@ import com.origemite.authserver.data.db.entity.TbUserPolicy;
 import com.origemite.authserver.data.db.entity.id.TbUserPolicyId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TbUserPolicyRepo extends JpaRepository<TbUserPolicy, TbUserPolicyId> {
+
+    List<TbUserPolicy> findByUsrId(String usrId);
 
 }
