6a20126cd8c2fa380e24c75797ede997764e0f3f
jangwonseok <jangws1003@naver.com>
2024-02-11 16:14:27 +0900

    feat(gateway) : 필터 추가

--- src/main/java/com/origemite/gateway/conf/ConfigFilter.java
+++ src/main/java/com/origemite/gateway/conf/ConfigFilter.java
@@ -1,6 +1,6 @@
 package com.origemite.gateway.conf;
 
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import com.origemite.gateway.filter.JwtAuthenticationFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +8,23 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigFilter {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public ConfigFilter(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
     //https://spring.io/projects/spring-cloud-gateway
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("auth-service", r -> r.path("/auth-service/**")
-                        .filters(f   -> f.rewritePath("/auth-service/(?<segment>.*)", "/${segment}"))
-                        .uri("lb://auth-service"))
+                .route("authserver", r -> r.path("/authserver/**")
+                        .filters(
+                                f   -> f
+                                        .rewritePath("/authserver/(?<segment>.*)", "/${segment}")
+                                        .filter(jwtAuthenticationFilter.apply(new JwtAuthenticationFilter.Config()))
+                        )
+                        .uri("lb://authserver"))
                 .build();
     }
 
