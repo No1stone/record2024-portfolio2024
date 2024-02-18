4ba039001a3538ba345cfb36f19b97790ba1980f
jangwonseok <jangws1003@naver.com>
2024-02-18 20:22:58 +0900

    feat(auth) : vo vaildation 추가

--- src/main/java/com/origemite/authserver/biz/controller/auth/vo/ReqSignin.java
+++ src/main/java/com/origemite/authserver/biz/controller/auth/vo/ReqSignin.java
@@ -1,6 +1,9 @@
 package com.origemite.authserver.biz.controller.auth.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,7 +12,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ReqSignin {
-//    @Email
+
+    @Schema(name = "usrEmail", description = "이메일", example = "abc@def.com", required = true, type = "String", maxLength = 50)
+    @Email(message = "이메일형식을 입력해주세요")
+    @NotBlank(message = "이메일은 필수값입니다.")
+    @Size(max = 50, message = "50자를 초과 할 수 없습니다.")
     private String email;
     private String password;
 
